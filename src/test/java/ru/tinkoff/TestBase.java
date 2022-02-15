dff2b8aacafdda229c373578ebc72344248359d9
andrey <andreiqa@protonmail.com>
2022-02-15 03:07:07 +0300

    add selenide support

---
+++ src/test/java/ru/tinkoff/TestBase.java
@@ -0,0 +1,39 @@
+package ru.tinkoff;
+
+import helpers.Attach;
+import com.codeborne.selenide.Configuration;
+//import com.codeborne.selenide.logevents.SelenideLogger;
+import com.codeborne.selenide.Selenide;
+import io.qameta.allure.Step;
+//import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class TestBase {
+
+    @BeforeAll
+    @Step("Конфигурируем браузер и удаленный запуск")
+    static void beforeAllMethod() {
+//        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+//        Configuration.remote = System.getProperty("remote_driver_url", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
+    }
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+        Selenide.closeWebDriver();
+    }
+}
